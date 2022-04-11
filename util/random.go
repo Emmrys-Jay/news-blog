746ae12eec40add008e9610684f4b9aa255c6768
Emmrys-Jay <jonathanemma121@gmail.com>
2022-04-11 14:35:38 +0100

    Completed functions used to generate random accounts, entries and transfers.

---
+++ util/random.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func RandomInt(n int64) int64 {
+	num := rand.Int63n(n)
+	return num
+}
+
+func RandomString(n int) string {
+	k := len(alphabet)
+	var s strings.Builder
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		s.WriteByte(c)
+	}
+
+	return s.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomBalance() int64 {
+	return RandomInt(10000)
+}
+
+func RandomCurrency() string {
+	c := []string{"EUR", "NGN", "USD", "JPY", "CHF"}
+	k := len(c)
+	n := rand.Intn(k)
+	return c[n]
+}
+
+func RandomEntryAmount() int64 {
+	num := RandomInt(10000)
+	s := []int{1, -1}
+
+	n := s[rand.Intn(len(s))]
+	num *= int64(n)
+	return num
+}
