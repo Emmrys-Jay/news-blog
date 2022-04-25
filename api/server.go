e82576a02604657417dad3d09aed1edfbcaef4a1
Emmrys-Jay <jonathanemma121@gmail.com>
2022-04-25 12:17:34 +0100

    Fixed bug in import statement

--- api/server.go
+++ api/server.go
@@ -1,25 +1,30 @@
 package api
 
 import (
-	"github.com/Emmrys-Jay/simple-bank/db"
+	db "github.com/Emmrys-Jay/simple-bank/db/sqlc"
 	"github.com/gin-gonic/gin"
 )
 
+// Server serves HTTP request for our banking service
 type Server struct {
 	store  *db.Store
 	router *gin.Engine
 }
 
+// NewServer creates a new HTTP server and setup routing
 func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts/", server.listAccounts)
 
 	server.router = router
 	return server
 }
 
+// Start runs the  http server on a specific address
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
